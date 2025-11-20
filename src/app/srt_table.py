cc778d7456a69f6b9f481b1ced609aeb68f42add
uselessgoddess <dodickgod@gmail.com>
2025-11-20 21:28:00 +0300

    make srt table sortable

--- src/app/srt_table.py
+++ src/app/srt_table.py
@@ -9,9 +9,19 @@ from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QColor
 
 from core.services import srt
+from src.ui.widgets import Switch
+from src.ui.theme import CURRENT_THEME
 
-from ui.widgets import Switch
-from ui.theme import CURRENT_THEME
+
+class SortableItem(QTableWidgetItem):
+  def __lt__(self, other):
+    val1 = self.data(Qt.ItemDataRole.UserRole)
+    val2 = other.data(Qt.ItemDataRole.UserRole)
+
+    if val1 is None or val2 is None:
+      return super().__lt__(other)
+
+    return val1 < val2
 
 
 class SRTTable(QTableWidget):
@@ -33,6 +43,8 @@ class SRTTable(QTableWidget):
     header.setSectionResizeMode(1, QHeaderView.ResizeMode.ResizeToContents)
     header.setSectionResizeMode(2, QHeaderView.ResizeMode.ResizeToContents)
 
+    self.setSortingEnabled(True)
+
     self.setStyleSheet(f"""
             QTableWidget {{ 
                 background-color: transparent; 
@@ -52,12 +64,10 @@ class SRTTable(QTableWidget):
         """)
 
   def populate(self, routes: list[srt.Route]):
-    TARGET_PING = 120  # TODO: research this
+    TARGET_PING = 120
 
+    self.setSortingEnabled(False)
     self.setRowCount(0)
-    # potential sorting
-    # routes.sort(key=lambda x: x.ping if x.ping > 0 else 9999)
-
     self.setRowCount(len(routes))
 
     for row, route in enumerate(routes):
@@ -66,11 +76,13 @@ class SRTTable(QTableWidget):
       self.setItem(row, 0, name_item)
 
       ping_text = f"{route.ping} ms" if route.ping >= 0 else "N/A"
-      ping_item = QTableWidgetItem(ping_text)
+      ping_item = SortableItem(ping_text)
+
+      sort_value = route.ping if route.ping >= 0 else 9999
+      ping_item.setData(Qt.ItemDataRole.UserRole, sort_value)
 
       if route.ping > 0:
         diff = abs(route.ping - TARGET_PING)
-
         if diff <= 20:
           color = QColor(CURRENT_THEME.ACCENT_GREEN)
         elif diff <= 40:
@@ -79,12 +91,16 @@ class SRTTable(QTableWidget):
           color = QColor("#fe8019")
         else:
           color = QColor(CURRENT_THEME.ACCENT_RED)
-
         ping_item.setForeground(color)
       else:
         ping_item.setForeground(QColor(CURRENT_THEME.SECONDARY_TEXT))
 
       self.setItem(row, 1, ping_item)
+
+      block_sort_value = 1 if route.blocked else 0
+      block_item = SortableItem()
+      block_item.setData(Qt.ItemDataRole.UserRole, block_sort_value)
+      self.setItem(row, 2, block_item)
 
       switch = Switch(checked=route.blocked)
       switch.toggled.connect(
@@ -97,3 +113,5 @@ class SRTTable(QTableWidget):
       layout.addWidget(switch)
       layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
       self.setCellWidget(row, 2, cell_widget)
+
+    self.setSortingEnabled(True)
